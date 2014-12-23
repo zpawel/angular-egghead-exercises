a7170dd7cf59134167e1793cf202db26814efa8a
Kronos66 <maksymilian.pamula@gmail.com>
2014-12-23 14:41:51 +0100

    complete

---
+++ app/app.js
@@ -0,0 +1,16 @@
+var provider = angular.module("provider", []);
+provider.config(function ($routeProvider) {
+
+});
+
+provider.factory('exercisePromise', function () {
+    var promise = null;
+    return {
+
+    }
+
+});
+
+provider.controller('appCtrl', function ($scope, exercisePromise) {
+
+});
