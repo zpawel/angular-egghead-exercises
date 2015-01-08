1e861273f24c41a5f09b8be6cee5ca5c8af7dad3
aniaw <ania_w@outlook.com>
2015-01-08 17:34:52 +0100

    Update exercise 18

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ The goal of this exercise is organizing controllers and directives by collecting
 * check how the application work before changes
 * add to ```Calc.js``` **calcAppElements** object, which will be a container of our controllers and directives
 * add to ```calcAppElements``` empty **controllers** and **directives** object
-* switch controllers and directors form as it was presented in the video
+* switch controllers and directives form the way it was presented in the video
 * at the end, add directives to ```calcApp.directive``` like this ```calcApp.directive(calcAppElements.directives);```
 * do the same with controllers
 * check how the application work after changes
--- app/Calc.js
+++ app/Calc.js
@@ -1,58 +1,65 @@
 var calcApp = angular.module("calcApp", []);
 
-calcApp.controller("SumCtrl", function ($scope)
-{
-    this.addition = function (a, b)
-    {
-        alert("a = " + a + "\nb = " + b + "\na + b = " + (a + b));
+calcApp.controller("SumCtrl", function ($scope) {
+    this.firstNumberSum = 5;
+    this.secondNumberSum = 2;
+
+    this.addition = function (a, b) {
+        this.sum = a + b;
     };
 
     return $scope.SumCtrl = this;
 
 });
 
-calcApp.controller("SubCtrl", function ($scope)
-{
-    this.subtraction = function (a, b)
-    {
-        alert("a = " + a + "\nb = " + b + "\na - b = " + (a - b));
+calcApp.controller("SubCtrl", function ($scope) {
+    this.firstNumberSub = 12;
+    this.secondNumberSub = 1;
+
+    this.subtraction = function (a, b) {
+        this.sub = a - b;
     };
     return $scope.SubCtrl = this;
 
 });
 
-calcApp.controller("MultiCtrl", function ($scope)
-{
-    $scope.multiplication = function (a, b)
-    {
-        alert("a = " + a + "\nb = " + b + "\na * b = " + (a * b));
+calcApp.controller("MultiCtrl", function ($scope) {
+    $scope.firstNumberMult = 12;
+    $scope.secondNumberMult = 2;
+
+    $scope.multiplication = function (a, b) {
+        $scope.multi = a * b;
     };
 });
 
-calcApp.controller("DivideCtrl", function ($scope)
-{
-    $scope.divide = function (a, b)
-    {
-        alert("a = " + a + "\nb = " + b + "\na / b = " + (a / b));
+calcApp.controller("DivideCtrl", function ($scope) {
+    $scope.firstNumberDiv = 15;
+    $scope.secondNumberDiv = 5;
+
+    $scope.divide = function (a, b) {
+        $scope.divid = a / b;
     };
 });
 
-calcApp.directive("multi", function ()
-{
+calcApp.directive("multi", function () {
     return{
         restrict: "E",
         scope: {
+            a: "@",
+            b: "@",
             solve: "&"
         },
         template: ' <div class="btn btn-info" ng-click="solve({a : a, b: b})">Multiple</div>'
+
     }
 });
 
-calcApp.directive("divide", function ()
-{
+calcApp.directive("divide", function () {
     return{
         restrict: "A",
         scope: {
+            a: "@",
+            b: "@",
             solve: "&"
         },
         template: '<div class="btn btn-default" ng-click="solve({a : a, b: b})">Divide</div>'
