f7a04fb740db5f37ab591f85b750a790aac7f94f
maksymilian.pamula <maksymilian.pamula@gmail.com>
2015-01-09 09:25:26 +0100

    update

--- app/connectDirective.js
+++ app/connectDirective.js
@@ -1,57 +1,65 @@
-var exercise = angular.module("exercise", ['ngRoute']);
+(function () {
+  'use strict';
 
-exercise.config(function ($routeProvider) {
-  //here writing routing
-});
-exercise.controller('app',function($scope){
-  $scope.$root.error=true;
-});
+  var exercise = angular.module("exercise", ['ngRoute']);
 
-exercise.controller('firstCtrl', function ($location) {
-  this.firstMessage="When you write 'first' you use function firstChange in firstCtrl.";
-  this.firstChange = function () {
-    $location.path('/first')
-  };
-});
+  exercise.config(function ($routeProvider) {
+    //here writing routing
 
-exercise.controller('secondCtrl', function ($location) {
-  this.secondMessage="When you write 'second' you use function secondChange in secondCtrl.";
-  this.secondChange = function () {
-    $location.path('/second')
-  }
-});
+  });
+  exercise.controller('app', function ($scope) {
+    $scope.$root.error = true;
+  });
 
-exercise.directive('firstDirective', function () {
-  return {
-    //set correct property
-  }
-});
+  exercise.controller('firstCtrl', function ($location) {
+    this.firstChange = function () {
+      $location.path('/first');
+    };
+  });
 
-exercise.directive('secondDirective', function () {
-  return {
-    //set correct property
-  }
-});
+  exercise.controller('secondCtrl', function ($location) {
+    this.secondMessage = "When you write 'first' you use function firstChange in firstCtrl.";
+    this.secondChange = function () {
+      $location.path('/second');
+    };
+  });
+  exercise.controller('thirdCtrl', function () {
+    this.thirdMessage='Good luck!';
+  });
 
-exercise.directive('thirdDirective', function ($location) {
-  return {
-    //set correct property
-    template:
-    '<div>' +
-    '<div class="text-muted">' +
-      //here display message
-    '</div>' +
-    '<div class="form-group">' +
-    '<label>Write something:</label>' +
-      //here add input
-    '</div>' +
-    '<div class="btn btn-success" ng-click="changeValue()">Change page</div>' +
-    '</div>',
-    link: function (scope, element, attrs, ctrls) {
-      //set correctly property and add function
-      scope.changeValue = function () {
+  exercise.directive('firstDirective', function () {
+    return {
+      //set correct property
+    };
+  });
 
+  exercise.directive('secondDirective', function () {
+    return {
+      //set correct property
+    };
+  });
+
+  exercise.directive('thirdDirective', function ($location) {
+    return {
+      //set correct property
+      restrict: 'E',
+      require: ['^firstDirective', '^secondDirective'],
+      controller:'thirdCtrl as third',
+      template: '<div>' +
+      '<div class="text-muted">' +
+        //here display message
+      '</div>' +
+      '<div class="form-group">' +
+      '<label>Write something:</label>' +
+        //here add input
+      '</div>' +
+      '<div class="btn btn-success" ng-click="changeValue()">Change page</div>' +
+      '</div>', link: function (scope, element, attrs, ctrls) {
+        //set correctly property and complete function
+        scope.changeValue = function () {
+
+        };
       }
-    }
-  }
-});
+    };
+  });
+})();
