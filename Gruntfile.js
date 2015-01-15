9f53946d4e6e30cad8cdb687ff3f37ea8a740805
Krzysztof Buchacz <k.buchacz@gmail.com>
2015-01-15 17:23:58 +0100

    Exercise 12

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,50 +9,50 @@
 
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-    var config = {
-        app: 'app'
-    };
+  var config = {
+    app: 'app'
+  };
 
-    grunt.initConfig({
-        config: config,
-        watch: {
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js'
-                ]
-            }
+  grunt.initConfig({
+    config: config,
+    watch: {
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
         },
-        connect: {
-            options: {
-                port: 9000,
-                livereload: 35729,
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    open: true,
-                    middleware: function (connect) {
-                        return [
-                            connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
+        files: [
+          '<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          middleware: function (connect) {
+            return [
+              connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
 
-                        ];
-                    }
-                }
-            }
+            ];
+          }
         }
-    });
+      }
+    }
+  });
 
-    grunt.registerTask('serve', function () {
-        grunt.task.run([
-            'connect:livereload', 'watch'
-        ]);
-    });
+  grunt.registerTask('serve', function () {
+    grunt.task.run([
+      'connect:livereload', 'watch'
+    ]);
+  });
 
-    grunt.registerTask('default', ['serve']);
+  grunt.registerTask('default', ['serve']);
 };
