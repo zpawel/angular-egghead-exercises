6bf75999e45e21ec950e72488e2538c4bd73322c
maksymilian.pamula <maksymilian.pamula@gmail.com>
2015-01-16 11:01:28 +0100

    all change

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 ##Exercise 23 : templateCache
 
 ##The goal
-Directive **url** has a big template, transfer it to another file
-is NOT optimal can you try resolve this problem another method.
+In the application, the same data can be displayed in several different method,
+depending on the given method is changed template in directive. All templates are ready and their behavior.
 
 
 ####Requirements
- * You cannot change functionality directive
- * You cannot change property in directive
- * The must be in **templateUrl.js** but NOT in directive
- * Delete **urlTemplate.html**
+ * You can NOT change **return** in directive.
+ * You have to add **template** to cache templates.
+ * Default template is display only *```order```*
 
+ ![Default view](images/defaultTemplate.png)
+
+ * When You write in the **template** *```button you should see```* a **button** *```when write something in the order```*
+
+ ![Button view](images/buttonTemplate.png)
+
+ * When You write **table**, You should be see a table with data
+
+ ![Table view](images/tableTemplate.png)
+
+ * Should NOT matter sizes characters
+
+ ![Should NOT matter sizes characters](images/tableTemplate2.png)
 
 ###Before you start, please refer to:
 * [angular templateCache](https://egghead.io/lessons/angularjs-templatecache)
+* [$compile](https://code.angularjs.org/1.2.16/docs/api/ng/service/$compile)
+* [$watch](https://egghead.io/lessons/angularjs-the-basics-of-scope-watch)
 
 
 Good luck!
--- app/templateUrl.js
+++ app/templateUrl.js
@@ -1,32 +1,47 @@
 (function ()
 {
   'use strict';
+  var dynamicChangeTemplate = angular.module('dynamicChangeTemplate', []);
 
-  var candyTemplateUrl = angular.module('templateUrl', []);
+  dynamicChangeTemplate.run(function(){
+    var template = [];
+    template[0] = '<div>Default template:</div>\n<div>{{order}}</div>';
+    template[1] = '<div>Button template:</div>\n<div>\n    <h3 ng-click="content()" class="btn btn-info" ng-if="isCandyVisible(order)">{{order}}</h3>\n    <div ng-show="isContentVisible"><div ng-if="order&&quantity > 100" class="form-group">\n        <span>You\'ve ordered a lot of {{order}}s!</span>\n    </div>\n        <div ng-if="quantity < 100 && quantity > 1" class="form-group">Your order: {{quantity}} {{order}}s</div>\n        <div ng-if="quantity == 1" class="form-group">Your order: {{quantity}} {{order}}</div>\n    </div>\n</div>';
+    template[2] = '<div>Table template:</div>\n<table class="table table-bordered">\n    <thead>\n    <tr>\n        <th class="col-md-1">Order:</th>\n        <th class="col-md-1">Quantity:</th>\n    </tr>\n    </thead>\n    <tbody>\n    <tr>\n        <td>{{order}}</td>\n        <td>{{quantity}}</td>\n    </tr>\n    </tbody>\n</table>';
+  });
 
-
-  candyTemplateUrl.directive('url', function ()
+  dynamicChangeTemplate.directive('url', function ($compile)
   {
+
+    var link = function (scope,element)
+    {
+      scope.$watch(function ()
+      {
+
+      }, function ()
+      {
+
+        $compile(element.contents())(scope);
+      });
+      scope.isContentVisible = false;
+
+      scope.isCandyVisible = function (value)
+      {
+        return !(!value || 0 === value.length);
+      };
+      scope.content = function ()
+      {
+        scope.isContentVisible = !scope.isContentVisible;
+      };
+    };
     return {
       restrict: 'E',
+      replace: true,
       scope: {
-        title: '@',
+        order: '@',
         quantity: '@'
       },
-      templateUrl: 'urlTemplate.html',
-      link: function (scope)
-      {
-        scope.isContentVisible = false;
-
-        scope.isCandyVisible = function (value)
-        {
-          return !(!value || 0 === value.length);
-        };
-        scope.content = function ()
-        {
-          scope.isContentVisible = !scope.isContentVisible;
-        };
-      }
+      link: link
     };
   });
 })();
