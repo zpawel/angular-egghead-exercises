14c9dcd6d32b982452762cb477280e79a6a0b5a5
aniaw <ania_w@outlook.com>
2015-01-16 12:01:12 +0100

    update exercise16

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ##Exercise 16 : Transclusion basics
+##The goal
 Discover the true nature of animals with ng-transclude directive. Update **animal** directive to display the content inside animal directives (dog, cat and pig). 
 
 ###Before you start, please refer to:
--- app/Animal.js
+++ app/Animal.js
@@ -11,9 +11,9 @@ animalApp.directive('animal', function ()
                 '<h3>{{name}}</h3>' +
                 '<div>' +
                 '<div ng-switch on="name">' +
-                '<div ng-switch-when="dog"><div class="btn btn-default"><img src="animals/dog.png"></div></div>' +
-                '<div ng-switch-when="cat"><div class="btn btn-default"><img src="animals/cat.png"></div></div>' +
-                '<div ng-switch-when="pig"><div class="btn btn-default"><img src="animals/pig.jpg"></div></div>' +
+                '<div ng-switch-when="dog"><div class="btn btn-default"><img src="assets/dog.png"></div></div>' +
+                '<div ng-switch-when="cat"><div class="btn btn-default"><img src="assets/cat.png"></div></div>' +
+                '<div ng-switch-when="pig"><div class="btn btn-default"><img src="assets/pig.jpg"></div></div>' +
                 '</div></div>'
     };
 });
