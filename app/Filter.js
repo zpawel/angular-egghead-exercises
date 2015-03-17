60212543f4a4f4acf0789f4568dda6addaf4186f
aniaw <ania_w@outlook.com>
2015-03-17 20:58:32 +0100

    standarization

--- app/Filter.js
+++ app/Filter.js
@@ -1,13 +1,15 @@
-var myApp = angular.module('myApp', []);
+'use strict';
+var app = angular.module('app', []);
 
-<!--add quote filter-->
-<!--add withoutH filter-->
-<!--add firsLetterUp filter-->
+//add quote filter
+//add withoutH filter
+//add firsLetterUp filter
 
-function FilterCtrl($scope) {
+app.controller('FilterCtrl', function ($scope)
+{
     $scope.data = {
-        citation: "To be, or not to be...",
-        title: "The Hobbit: The Battle of the Five Armies",
-        someText: "Visit BBC News for up-to-the-minute news, breaking news, video, audio and stories."
-    }
-}
+        citation: 'To be, or not to be...',
+        title: 'The Hobbit: The Battle of the Five Armies',
+        someText: 'Visit BBC News for up-to-the-minute news, breaking news, video, audio and stories.'
+    };
+});
