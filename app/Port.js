6114481959bf7008d49d815faaf5757b7d11c654
aniaw <ania_w@outlook.com>
2015-03-18 22:44:54 +0100

    standarization

--- app/Port.js
+++ app/Port.js
@@ -1,35 +1,37 @@
+'use strict';
 var app = angular.module('weatherApp');
 
-app.controller("shipCtrl", function ($scope, WeatherConditions)
+app.controller('shipCtrl', function ($scope, WeatherConditions)
 {
 //  checkMyBoat function should be used to check if boat is able to depart from port
-  $scope.checkMyBoat = function (boat, conditions) {
-    switch(conditions) {
-      case 'stormy':
+    $scope.checkMyBoat = function (boat, conditions)
+    {
+        switch (conditions) {
+            case 'stormy':
+                return false;
+            case 'windy':
+            case 'heavy rain':
+                return boat !== 'Sloop';
+            case 'cloudy':
+            case 'clear':
+            case 'sunny':
+                return true;
+            default:
+                return false;
+        }
         return false;
-      case 'windy':
-      case 'heavy rain':
-        return boat === 'Sloop' ? false : true;
-      case 'cloudy':
-      case 'clear':
-      case 'sunny':
-        return true;
-      default:
-        return false;
-    }
-    return false;
-  };
-  
+    };
+
 //  weatherConditions represent actual conditions API object. Use getCurrentConditions() method to receive current conditions
-  $scope.weatherConditions = WeatherConditions;
+    $scope.weatherConditions = WeatherConditions;
 });
 
-app.directive('shipSender', function () {
-  return {
-    template: '<div class="ship relative">'
-        + '<div class="bubble" ng-if="answer">Set sails!</div>'
-//        value from the select element below should be used by captain to set boat model and passed as argument to correct function
-        + '<div class="col-md-offset-1 col-md-10 align-bottom"><select class="form-control" ng-options="item for item in [\'Sloop\', \'Ship\', \'Towboat\', \'Brig\']"></select>'
-        + '<div class="btn btn-info voffset">Check<!--check button--></div></div></div>'
-  }
+app.directive('shipSender', function ()
+{
+    return {
+        template: '<div class="ship relative">' +
+                '<div class="bubble" ng-if="answer">Set sails!</div>' + //        value from the select element below should be used by captain to set boat model and passed as argument to correct function
+                '<div class="col-md-offset-1 col-md-10 align-bottom"><select class="form-control" ng-options="item for item in [\'Sloop\', \'Ship\', \'Towboat\', \'Brig\']"></select>' +
+                '<div class="btn btn-info voffset">Check<!--check button--></div></div></div>'
+    };
 });
