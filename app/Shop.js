a0346cbcb430296637edc9d244706239bb024d31
aniaw <ania_w@outlook.com>
2015-03-20 21:04:34 +0100

    standarization

--- app/Shop.js
+++ app/Shop.js
@@ -1,34 +1,36 @@
-var shopApp = angular.module("shopApp", []);
+'use strict';
 
-shopApp.controller("appCtrl", function ($scope)
+var app = angular.module('app', []);
+
+app.controller('appCtrl', function ($scope)
 {
-  var sweets = ['Sugar Daddy', 'Nerds', 'Jawbreakers', 'Starburst', 'Milky Way', 'Skittles', 'Jelly Belly'];
-  var cakes = ['Banoffee pie', 'Brownie', 'Butter cake', 'Chocolate cake', 'Cupcake', 'Karpatka'];
-  $scope.sweets = [];
-  $scope.cakes = [];
-  $scope.cart = [];
+    var sweets = ['Sugar Daddy', 'Nerds', 'Jawbreakers', 'Starburst', 'Milky Way', 'Skittles', 'Jelly Belly'];
+    var cakes = ['Banoffee pie', 'Brownie', 'Butter cake', 'Chocolate cake', 'Cupcake', 'Karpatka'];
+    $scope.sweets = [];
+    $scope.cakes = [];
+    $scope.cart = [];
 
-  angular.forEach(sweets, function (sweet)
-  {
-    $scope.sweets.push({name: sweet});
-  });
-  angular.forEach(cakes, function (cake)
-  {
-    $scope.cakes.push({name: cake});
-  });
+    angular.forEach(sweets, function (sweet)
+    {
+        $scope.sweets.push({name: sweet});
+    });
+    angular.forEach(cakes, function (cake)
+    {
+        $scope.cakes.push({name: cake});
+    });
 
-  $scope.buyProduct = function (product)
-  {
-    product != null ? $scope.cart.push({name: product}) : false;
-  }
+    $scope.buyProduct = function (product)
+    {
+        product != null ? $scope.cart.push({name: product}) : false;
+    };
 });
 
-shopApp.directive("orderComponent", function ()
+app.directive('orderComponent', function ()
 {
-  var template = '<div class="panel panel-success text-center"><p class="panel-heading"><strong>I want to buy:</strong></p><div class="panel-body">'
-      + '<select class="form-control form-group"><!--select element--></select>'
-      + '<div class="btn btn-success"><!--the bottom div-->Buy!</div></div></div>';
-  return {
-    template: template
-  }
+    var template = '<div class="panel panel-success text-center"><p class="panel-heading"><strong>I want to buy:</strong></p><div class="panel-body">' +
+            '<select class="form-control form-group"><!--select element--></select>' +
+            '<div class="btn btn-success"><!--the bottom div-->Buy!</div></div></div>';
+    return {
+        template: template
+    };
 });
