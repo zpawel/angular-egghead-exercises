bb11848d39f4472694d3802bc35a38f5d790b012
aniaw <ania_w@outlook.com>
2015-04-14 12:52:07 +0200

    standarization

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 ##Exercise 22 : templateUrl
 
-##The goal
 Directive **url** has a big template, try resolve this problem by moving it to another file.
 
+###Requirements
+ * you can NOT change functionality directive
+ * you can NOT change exist properties in directive
 
-####Requirements
- * You can NOT change functionality directive
- * You can NOT change exist properties in directive
- * Create new file in **app** and ***set the correct name***
+###Results
+application should work the same as before the change
+
+* type tex in **title**
+
+![alt text](app/assets/1.png "1")
+
+* type text in **content**, click the button
+
+![alt text](app/assets/2.png "2")
+
+* type ***secret*** in **title** field
+
+![alt text](app/assets/3.png "3")
 
 
 ###Before you start, please refer to:
-* [templateurl](https://egghead.io/lessons/angularjs-templateurl)
-
+* [angularjs-templateurl](https://egghead.io/lessons/angularjs-templateurl)
 
 Good luck!
--- app/templateUrl.js
+++ app/templateUrl.js
@@ -1,27 +1,24 @@
-(function ()
-{
-  'use strict';
+'use strict';
+var app = angular.module('app', []);
 
-  var templateUrl = angular.module('templateUrl', []);
-  templateUrl.directive('url', function ()
-  {
+app.directive('url', function ()
+{
     return {
-      restrict: 'E',
-      scope: {
-        title: '@',
-        data: '@'
-      }, templateUrl: 'urlTemplate.html',
-      link: function (scope)
-      {
-        scope.isTitleVisible = function (value)
+        restrict: 'E',
+        scope: {
+            title: '@', data: '@'
+        },
+        template: '<button ng-click="content()" class="btn btn-info" ng-if="isTitleVisible(title)">\n    {{title}}\n</button>\n<div class="form-group" ng-show="isContentVisible">\n    <span ng-if="\'secret\'!==title">The data is: {{data}}</span>\n    <span class="text-success" ng-if="\'secret\'===title">Good, work you display this secret</span>\n</div>\n',
+        link: function (scope)
         {
-          return !(!value || 0 === value.length);
-        };
-        scope.content = function ()
-        {
-          scope.isContentVisible = !scope.isContentVisible;
-        };
-      }
+            scope.isTitleVisible = function (value)
+            {
+                return !(!value || 0 === value.length);
+            };
+            scope.content = function ()
+            {
+                scope.isContentVisible = !scope.isContentVisible;
+            };
+        }
     };
-  });
-})();
+});
