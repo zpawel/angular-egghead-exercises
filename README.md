d822d61ef707e2822b44daa81cafb1e25c29a4f4
aniaw <ania_w@outlook.com>
2015-04-14 11:51:31 +0200

    standarization

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 ##Exercise 21 : scope vs $scope
 
-##The goal
 In this application existed two directives on the same view, but in here we encountered the problem.
 When complement one ```input``` automatically completes the second input.
 
-####Requirements
- * You can NOT change the **templates** directives.
- * You can NOT change the **index.html**.
- * You should be able to insert two different values for the inputs and display it by clicking on the corresponding button.
-
+###Requirements
+ * you can NOT change the **templates** directives.
+ * you can NOT change the **index.html**.
+ * you should be able to insert two different values for the inputs and display it by clicking on the corresponding button.
 
 ###Before you start, please refer to:
-* [scope vs scope](https://egghead.io/lessons/angularjs-scope-vs-scope)
+* [angularjs-scope-vs-scope](https://egghead.io/lessons/angularjs-scope-vs-scope)
 
 Good luck!
--- app/scope.js
+++ app/scope.js
@@ -1,48 +1,55 @@
-(function () {
-  'use strict';
+(function ()
+{
+    'use strict';
 
-  var appScope = angular.module('appScope', []);
+    var appScope = angular.module('appScope', []);
 
-  appScope.controller('scopeCtrl', ['$scope', 'simpleFactory', function ($scope, simpleFactory) {
-    this.unitScope = function () {
-      this.unitScopeValue = $scope.input;
-    };
-    this.separateScope = function () {
-      this.otherScopeValue = simpleFactory.get();
-    };
-    $scope.ctrl = this;
-  }]);
+    appScope.controller('scopeCtrl', function ($scope, simpleFactory)
+    {
+        this.unitScope = function ()
+        {
+            this.unitScopeValue = $scope.input;
+        };
+        this.separateScope = function ()
+        {
+            this.otherScopeValue = simpleFactory.get();
+        };
+        $scope.ctrl = this;
+    });
 
-  appScope.factory('simpleFactory', function () {
-    var valueFromScope;
-    return {
-      set: function (value) {
-        valueFromScope = value;
-      },
-      get: function () {
-        return valueFromScope;
-      }
-    };
-  });
+    appScope.factory('simpleFactory', function ()
+    {
+        var valueFromScope;
+        return {
+            set: function (value)
+            {
+                valueFromScope = value;
+            }, get: function ()
+            {
+                return valueFromScope;
+            }
+        };
+    });
 
-  appScope.directive('unit', function () {
-    return {
-      restrict: 'E',
-      replace: true,
-      template: '<div> <input class="form-group" type="text" ng-model="input"></div>'
-    };
-  });
+    appScope.directive('unit', function ()
+    {
+        return {
+            restrict: 'E', replace: true, template: '<div> <input class="form-control"type="text" ng-model="input"></div>'
+        };
+    });
 
-  appScope.directive('separate', function (simpleFactory) {
-    return {
-      restrict: 'E',
-      replace: true,
-      template: '<div> <input class="form-group" type="text" ng-model="input"></div>',
-      link: function (scope) {
-        scope.$watch('input', function (newValue) {
-          simpleFactory.set(newValue);
-        });
-      }
-    };
-  });
+    appScope.directive('separate', function (simpleFactory)
+    {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div> <input class="form-control" type="text" ng-model="input"></div>', link: function (scope)
+            {
+                scope.$watch('input', function (newValue)
+                {
+                    simpleFactory.set(newValue);
+                });
+            }
+        };
+    });
 })();
