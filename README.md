c2f5f1d82979657a7400424d13e71ffca86baa45
aniaw <ania_w@outlook.com>
2015-04-15 09:27:32 +0200

    standarization

--- README.md
+++ README.md
@@ -1,28 +1,29 @@
 ##Exercise 23 : templateCache
 
-##The goal
 In the application, the same data can be displayed in several different method,
-depending on the given method is changed template in directive. All templates are ready and their behavior.
+depending on the given method is changed template in directive. All templates are ready.
 
+###Requirements
+ * DON'T change **return** in directive.
+ * add **template** to cache templates.
 
-####Requirements
- * You can NOT change **return** in directive.
- * You have to add **template** to cache templates.
- * Default template is display only *```order```*
+###Results
+* default view
 
- ![Default view](images/defaultTemplate.png)
+![alt text](app/assets/1.png "1")
 
- * When You write in the **template** *```button you should see```* a **button** *```when write something in the order```*
+* when you write ***button*** in the **template**, you should see the button
 
- ![Button view](images/buttonTemplate.png)
+![alt text](app/assets/2.png "2")
 
- * When You write **table**, You should be see a table with data
+* when you click on the button
 
- ![Table view](images/tableTemplate.png)
+![alt text](app/assets/3.png "3")
 
- * Should NOT matter sizes characters
-
- ![Should NOT matter sizes characters](images/tableTemplate2.png)
+* when you write ***table*** in the **template**, you should be see a table with data
+ 
+ ![alt text](app/assets/4.png "4")
+ 
 
 ###Before you start, please refer to:
 * [angular templateCache](https://egghead.io/lessons/angularjs-templatecache)
--- app/templateUrl.js
+++ app/templateUrl.js
@@ -1,47 +1,51 @@
 (function ()
 {
-  'use strict';
-  var dynamicChangeTemplate = angular.module('dynamicChangeTemplate', []);
+    'use strict';
+    var app = angular.module('app', []);
 
-  dynamicChangeTemplate.run(function(){
-    var template = [];
-    template[0] = '<div>Default template:</div>\n<div>{{order}}</div>';
-    template[1] = '<div>Button template:</div>\n<div>\n    <h3 ng-click="content()" class="btn btn-info" ng-if="isCandyVisible(order)">{{order}}</h3>\n    <div ng-show="isContentVisible"><div ng-if="order&&quantity > 100" class="form-group">\n        <span>You\'ve ordered a lot of {{order}}s!</span>\n    </div>\n        <div ng-if="quantity < 100 && quantity > 1" class="form-group">Your order: {{quantity}} {{order}}s</div>\n        <div ng-if="quantity == 1" class="form-group">Your order: {{quantity}} {{order}}</div>\n    </div>\n</div>';
-    template[2] = '<div>Table template:</div>\n<table class="table table-bordered">\n    <thead>\n    <tr>\n        <th class="col-md-1">Order:</th>\n        <th class="col-md-1">Quantity:</th>\n    </tr>\n    </thead>\n    <tbody>\n    <tr>\n        <td>{{order}}</td>\n        <td>{{quantity}}</td>\n    </tr>\n    </tbody>\n</table>';
-  });
-
-  dynamicChangeTemplate.directive('url', function ($compile)
-  {
-
-    var link = function (scope,element)
+    app.run(function ()
     {
-      scope.$watch(function ()
-      {
+        var template = [];
+        template[0] = '<div>Default template:</div>\n<h3>{{order}} x {{quantity}}</h3>';
+        template[1] =
+                '<div>Button template:</div>\n<div>\n    <h3 ng-click="content()" class="btn btn-default">Click to see the order</h3>\n    <div ng-show="isContentVisible">\n        <h3 ng-if="quantity > 1" class="form-group">Your order: {{quantity}} {{order}}s</h3>\n        <h3 ng-if="quantity == 1" class="form-group">Your order: {{quantity}} {{order}}</h3>\n    </div>\n</div>';
+        template[2] =
+                '<h4>Table template:</h4>\n<table class="table table-bordered">\n    <thead>\n    <tr>\n        <th class="text-center">Order</th>\n        <th class="text-center">Quantity</th>\n    </tr>\n    </thead>\n    <tbody>\n    <tr>\n        <td>{{order}}</td>\n        <td>{{quantity}}</td>\n    </tr>\n    </tbody>\n</table>';
+    });
 
-      }, function ()
-      {
+    app.controller('urlCtrl', function ($scope)
+    {
+        $scope.order = 'pizza';
+        $scope.quantity = 12;
 
-        $compile(element.contents())(scope);
-      });
-      scope.isContentVisible = false;
+    });
+    app.directive('url', function ($compile)
+    {
 
-      scope.isCandyVisible = function (value)
-      {
-        return !(!value || 0 === value.length);
-      };
-      scope.content = function ()
-      {
-        scope.isContentVisible = !scope.isContentVisible;
-      };
-    };
-    return {
-      restrict: 'E',
-      replace: true,
-      scope: {
-        order: '@',
-        quantity: '@'
-      },
-      link: link
-    };
-  });
+        var link = function (scope, element)
+        {
+            scope.$watch(function ()
+            {
+
+            }, function ()
+            {
+
+                $compile(element.contents())(scope);
+            });
+            scope.isContentVisible = false;
+
+            scope.content = function ()
+            {
+                scope.isContentVisible = !scope.isContentVisible;
+            };
+        };
+
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                order: '@', quantity: '@'
+            }, link: link
+        };
+    });
 })();
