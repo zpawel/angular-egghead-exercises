67927f60485f5b8c1b4397e7eaca11312c8af1fe
aniaw <ania_w@outlook.com>
2015-04-20 08:20:47 +0200

    standarization

--- app/song.js
+++ app/song.js
@@ -1,19 +1,22 @@
-var songApp = angular.module("songApp", []);
+'use strict';
 
-songApp.factory("song", function () {
+var songApp = angular.module('songApp', []);
+
+songApp.factory('song', function ()
+{
     return {
-        author: "Gotye feat. Kimbra",
-        title: "Somebody That I Used To Know",
-        sing: function () {
-            return "But you didn't have to cut me off... But you treat me like a stranger ..."
-        },
-        fake: function () {
-            return "Buuuuuuuuuuuuuut you niah niah blah bla ..."
+        author: 'Gotye feat. Kimbra', title: 'Somebody That I Used To Know', sing: function ()
+        {
+            return 'But you did not have to cut me off... But you treat me like a stranger ...';
+        }, fake: function ()
+        {
+            return 'Buuuuuuuuuuuuuut you niah niah blah bla ...';
         }
-    }
+    };
 });
 
-songApp.controller("SongCtrl", function ($scope, song) {
+songApp.controller('SongCtrl', function ($scope, song)
+{
     $scope.author = song.author;
     $scope.title = song.title;
     $scope.sing = song.sing();
