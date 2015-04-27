2c221ef5c867b7ce950829b04e085aefadff017a
ania <ania_w@outlook.com>
2015-04-27 13:48:23 +0200

    standarization

--- README.md
+++ README.md
@@ -1,32 +1,23 @@
 ##Exercise 36b: ng-repeat-start
 The goal of this exercise is practise ng-repeat-start
 
+###Requirements 
+* don't change table structure
+* use ng-repeat-start directive to achieve the effect as in section
+* check out comments
+
+###Results
+
+* start view
+
+![alt text](app/assets/1.png, "1")
+
+* click at user
+
+![alt text](app/assets/1.png, "1")
+
+
 ###Before you start, please refer to:
 * [angularjs-ng-repeat-start](https://egghead.io/lessons/angularjs-ng-repeat-start)
-
-###Exercise
-
-1. Write routing
-    * when path is ```/``` display ```start.html``` with ```app``` controller
-2. index.html
-    * add **appCtrl as app**
-    * use **ng-view** to routing
-3. in start.js
-    * write routing
-        * when path is ```/``` display ```start.html``` with ```app``` controller
-    * in simpleFactory
-        * complete method get you should return object with property ```list``` and value ```listUser```
-    * in ```viewCtrl``` controller
-        * append``` listUsers``` from factory to ```user``` on controller
-        * and you should ```toggle``` enable element in the ```users.list```
-
-4. in start.html
-    * you can NOT change structure table!
-    * in first **div** add controller **viewCtrl as view**
-    * complete definition table
-        * use **ng-click** on the row table to toggle view country in next row
-        * use **ng-if** to display country
-
-
 
 Good luck!
--- app/start.js
+++ app/start.js
@@ -1,49 +1,53 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  var start = angular.module('start', ['ngRoute']);
+    var app = angular.module('app', []);
 
-  start.config(function ($routeProvider, $provide) {
-    //here writing routing
+    app.factory('UsersList', function () {
+        var usersList = {};
+        usersList.list = [
+            {
+                name: 'Adam', lastName: 'Hart', country: 'Poland'
+            },
+            {
+                name: 'Antonio', lastName: 'Turner', country: 'England'
+            },
+            {
+                name: 'Jimmy', lastName: 'Wright', country: 'USA'
+            },
+            {
+                name: 'Shirley', lastName: 'Harper', country: 'Italian'
+            },
+            {
+                name: 'Randy', lastName: 'Watson', country: 'Greece'
+            },
+            {
+                name: 'Gregory', lastName: 'Banks', country: 'German'
+            },
+            {
+                name: 'Martha', lastName: 'Lawson', country: 'China'
+            },
+            {
+                name: 'Eugene', lastName: 'Stanley', country: 'Cuba'
+            },
+            {
+                name: 'Randy', lastName: 'Reyes', country: 'Egypt'
+            },
+            {
+                name: 'Christopher', lastName: 'Ferguson', country: 'France'
+            }
+        ];
 
-    $provide.factory('simpleFactory', function () {
-      var listUser=[{
-                      name: 'Adam', lastName: 'Hart',country:'Poland'
-                    }, {
-                      name: 'Antonio', lastName: 'Turner',country:'England'
-                    }, {
-                      name: 'Jimmy', lastName: 'Wright',country:'USA'
-                    }, {
-                      name: 'Shirley', lastName: 'Harper',country:'Italian'
-                    }, {
-                      name: 'Randy', lastName: 'Watson',country:'Greece'
-                    }, {
-                      name: 'Gregory', lastName: 'Banks',country:'German'
-                    }, {
-                      name: 'Martha', lastName: 'Lawson',country:'China'
-                    }, {
-                      name: 'Eugene', lastName: 'Stanley',country:'Cuba'
-                    }, {
-                      name: 'Randy', lastName: 'Reyes',country:'Egypt'
-                    }, {
-                      name: 'Christopher', lastName: 'Ferguson',country:'France'
-                    }];
-      return {
-        get:function(){
-          //return object list with value listUser
-        }
-      };
+        return usersList;
+
     });
-  });
 
-  start.controller('appCtrl', function ($scope) {
-    $scope.$root.error = true;
-  });
-  start.controller('viewCtrl',function(simpleFactory){
-    //append listUsers from factory to user
-    this.toggle=function(index){
-      //complete function
-    };
-  });
-
-})();
+    app.controller('ViewCtrl', function (UsersList) {
+        this.users = UsersList;
+        this.toggle = function (index) {
+            //complete function
+            this.users.list[index].enable = !this.users.list[index].enable;
+        };
+    });
+})
+();
