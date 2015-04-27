3752b7a14e851beb11b8bd95b64fa086badd51f6
ania <ania_w@outlook.com>
2015-04-27 11:23:52 +0200

    standarization

--- app/vehicle.js
+++ app/vehicle.js
@@ -1,117 +1,99 @@
-var app = angular.module("app", []);
+'use strict';
+var app = angular.module('app', []);
 
-app.directive("vehicle", function ()
-{
+app.directive('vehicle', function () {
     return {
-        restrict: "E",
+        restrict: 'E',
         transclude: true,
-        controller: function ()
-        {
-            this.drive = function (speed, vehicle)
-            {
-                return "I'm fast " + vehicle + "! " + speed + " km/h! Whoa!";
+        controller: function () {
+            this.drive = function (speed, vehicle) {
+                return 'I am fast ' + vehicle + '! ' + speed + ' km/h! Whoa!';
             };
 
-            this.alarm = function ()
-            {
-                alert("Beeeeeeep!");
+            this.alarm = function () {
+                alert('Beeeeeeep!');
             };
 
         },
         template: '<div ng-transclude></div>'
-    }
+    };
 });
 
-app.directive("car", function ()
-{
+app.directive('car', function () {
     return {
-        restrict: "E",
-        require: "^vehicle",
+        restrict: 'E',
+        require: '^vehicle',
         transclude: true,
-        controller: function ()
-        {
+        controller: function () {
             this.emergencyBrake = false;
         },
-        link: function (scope, element, attrs, clientCtrl)
-        {
+        link: function (scope, element, attrs, clientCtrl) {
         },
         template: '<div class="col-md-6">' +
-                '<img class="text-center" src="assets/car.png">' +
-                '<div class="page-header"><strong>You are in the car</strong></div>' +
-                '<div ng-transclude></div></div>'
-    }
+            '<img class="text-center" src="assets/car.png">' +
+            '<div class="page-header"><strong>You are in the car</strong></div>' +
+            '<div ng-transclude></div></div>'
+    };
 });
 
-app.directive("bus", function ()
-{
+app.directive('bus', function () {
     return {
-        restrict: "E",
-        require: "^vehicle",
+        restrict: 'E',
+        require: '^vehicle',
         transclude: true,
-        controller: function ()
-        {
+        controller: function () {
             this.emergencyBrake = true;
 
         },
-        link: function (scope, element, attrs, clientCtrl)
-        {
+        link: function (scope, element, attrs, clientCtrl) {
 
         },
         template: '<div class="col-md-6">' +
-                '<img class="text-center" src="assets/bus.png">' +
-                '<div class="page-header"><strong>You are in the bus</strong></div >' +
-                '<div ng-transclude></div></div>'
+            '<img class="text-center" src="assets/bus.png">' +
+            '<div class="page-header"><strong>You are in the bus</strong></div >' +
+            '<div ng-transclude></div></div>'
 
-    }
+    };
 });
 
-app.directive("audi", function ()
-{
+app.directive('audi', function () {
     return {
-        restrict: "E",
-        link: function (scope, element, attrs, ctrls)
-        {
+        restrict: 'E',
+        link: function (scope, element, attrs, ctrls) {
             scope.speed = 80;
-            scope.setSpeed = function (speed)
-            {
+            scope.setSpeed = function (speed) {
 //                scope.driveSpeed
             };
-            scope.isClicked = function ()
-            {
+            scope.isClicked = function () {
 
             };
         },
         template: '<div class="col-md-4">' +
-                '<input class="form-control" type="number" ng-model="speed"></div>' +
-                '<div class="btn-group"><button class="btn btn-default" ng-click="setSpeed(speed)">Drive!</button>' +
-                '<button class="btn btn-default">Emergency brake!</button></div><h3></h3>'
+            '<input class="form-control" type="number" ng-model="speed"></div>' +
+            '<div class="btn-group"><button class="btn btn-default" ng-click="setSpeed(speed)">Drive!</button>' +
+            '<button class="btn btn-default">Emergency brake!</button></div><h3></h3>'
 
-    }
+    };
 });
 
-app.directive("jelcz", function ()
-{
+app.directive('jelcz', function () {
     return {
-        restrict: "E",
-        controller: function ()
-        {
+        restrict: 'E',
+        controller: function () {
         },
-        link: function (scope, element, attrs, ctrls)
-        {
+        link: function (scope, element, attrs, ctrls) {
             scope.speed = 12;
-            scope.setSpeed = function (speed)
-            {
+            scope.setSpeed = function (speed) {
 //                scope.driveSpeed
             };
 
-            scope.isClicked = function ()
-            {
+            scope.isClicked = function () {
 
             };
         },
         template: '<div class="col-md-4"><input class="form-control" type="number" ng-model="speed"></div>' +
-                '<div class="btn-group"><button class="btn btn-default" ng-click="setSpeed(speed)">Drive!</button>' +
-                '<button class="btn btn-default">Emergency brake!</button></div><h3></h3>'
+            '<div class="btn-group"><button class="btn btn-default" ng-click="setSpeed(speed)">Drive!</button>' +
+            '<button class="btn btn-default">Emergency brake!</button></div><h3></h3>'
 
-    }
+    };
 });
