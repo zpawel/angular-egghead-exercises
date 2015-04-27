b15fbf78e7b3f78e8edab55c73c3888030c1952b
ania <ania_w@outlook.com>
2015-04-27 10:48:02 +0200

    standatization

--- app/app.js
+++ app/app.js
@@ -1,49 +1,39 @@
 'use strict';
 var app = angular.module('app', []);
 
-app.service('songService', function ($http)
-{
+app.service('songService', function ($http) {
     return{
 
-        get: function ()
-        {
+        get: function () {
             return  $http.get('http://localhost:3000/favourite_songs');
         },
-        add: function (favouriteSong)
-        {
+        add: function (favouriteSong) {
             return $http.post('http://localhost:3000/favourite_songs', favouriteSong);
         },
-        remove: function ()
-        {
+        remove: function () {
             return $http.delete('http://localhost:3000/favourite_songs');
         }
     };
 });
 
 
-app.controller('AppCtrl', function ($http, songService)
-{
+app.controller('AppCtrl', function ($http, songService) {
     var ctrl = this;
 
-    songService.get().success(function (data)
-    {
+    songService.get().success(function (data) {
         ctrl.favouriteSongs = data;
     });
 
-    ctrl.addFavouriteSong = function (favouriteSong)
-    {
-        songService.add(favouriteSong).success(function (data)
-        {
+    ctrl.addFavouriteSong = function (favouriteSong) {
+        songService.add(favouriteSong).success(function (data) {
             ctrl.favouriteSongs = data;
             ctrl.favouriteSong = {artist: '', title: ''};
 
         });
     };
 
-    ctrl.removeFavouriteSongs = function ()
-    {
-        songService.remove().success(function (data)
-        {
+    ctrl.removeFavouriteSongs = function () {
+        songService.remove().success(function (data) {
             ctrl.favouriteSongs = data;
         });
 
