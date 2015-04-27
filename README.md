968b3412b1100e274bffa8a5f83d877eb345fbf7
ania <ania_w@outlook.com>
2015-04-27 13:19:50 +0200

    standarization

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ When you're hungry, you are not yourself. Check out, what your friends keep in t
 
 ###Result
 
-![alt text](app/assets/fridgeList.jpg "fridge list")
+![alt text](app/assets/1.png "fridge list")
 
 ###Before you start, please refer to:
 * [angularjs-ng-repeat-start](https://egghead.io/lessons/angularjs-ng-repeat-start)
--- app/fridge.js
+++ app/fridge.js
@@ -1,26 +1,27 @@
-var fridgeApp = angular.module("fridgeApp", []);
+'use strict';
 
-fridgeApp.controller("FridgeCtrl", function ()
-{
+var fridgeApp = angular.module('fridgeApp', []);
+
+fridgeApp.controller('FridgeCtrl', function () {
     this.list = [
-        { id: 1, name: "Austin", fridge: [
-            {name: "banana", count: 4 },
-            {name: "ham", count: 1 },
-            {name: "tomato", count: 1}
+        { id: 1, name: 'Austin', fridge: [
+            {name: 'banana', count: 4 },
+            {name: 'ham', count: 1 },
+            {name: 'tomato', count: 1}
         ]},
-        { id: 2, name: "Ben", fridge: [
-            {name: "banana", count: 4 },
-            {name: "wasabi", count: 2 },
-            {name: "ham", count: 1 },
-            {name: "cheese", count: 2}
+        { id: 2, name: 'Ben', fridge: [
+            {name: 'banana', count: 4 },
+            {name: 'wasabi', count: 2 },
+            {name: 'ham', count: 1 },
+            {name: 'cheese', count: 2}
         ]},
-        { id: 3, name: "Willy", fridge: [
-            {name: "milk", count: 10}
+        { id: 3, name: 'Willy', fridge: [
+            {name: 'milk', count: 10}
         ]},
-        { id: 4, name: "Maxine", fridge: [
-            {name: "tomato", count: 1},
-            {name: "milk", count: 3 },
-            {name: "butter", count: 2 }
+        { id: 4, name: 'Maxine', fridge: [
+            {name: 'tomato', count: 1},
+            {name: 'milk', count: 3 },
+            {name: 'butter', count: 2 }
         ]}
     ];
 });
