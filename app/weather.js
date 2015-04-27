2ef83f3f15e6aca1b6466bfe07f93b3c358cd676
ania <ania_w@outlook.com>
2015-04-27 10:30:59 +0200

    standarization

--- app/weather.js
+++ app/weather.js
@@ -1,7 +1,8 @@
+'use strict';
+
 var weatherApp = angular.module('weatherApp', []);
 
-weatherApp.controller('WeatherCtrl', function ($scope)
-{
+weatherApp.controller('WeatherCtrl', function ($scope) {
     $scope.weatherForecast = [
         { city: 'Krakow', temperature: '34°', describe: 'Partly sunny'},
         { city: 'Warsaw', temperature: '30°', describe: 'Dense fog'},
@@ -10,20 +11,17 @@ weatherApp.controller('WeatherCtrl', function ($scope)
 
     $scope.newWeather = { city: 'Katowice', temperature: '26°', describe: 'Sunny'};
 
-    $scope.addWeather = function (weather)
-    {
+    $scope.addWeather = function (weather) {
         $scope.weatherForecast.push(weather);
         $scope.newWeather = {};
     };
 
     $scope.currentTemp = 29;
-    $scope.increaseTemp = function ()
-    {
+    $scope.increaseTemp = function () {
         $scope.currentTemp += 1;
     };
 
-    $scope.decreaseTemp = function ()
-    {
+    $scope.decreaseTemp = function () {
         $scope.currentTemp += -1;
     };
 });
