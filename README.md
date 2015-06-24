5e728a986a85a849869bade3892eb2b8d65c8fd5
aniaw <ania_w@outlook.com>
2015-06-24 10:51:23 +0200

    add unit tests, update README

--- README.md
+++ README.md
@@ -1,23 +1,45 @@
-##Exercise 8 : Angular first directive
+#Exercise 8 : Angular first directive
 
+##Summary
 The world need more heroes. You are given a hard task to create at least two heroes and one superhero. Implement three simple directives that will
 allow you to create as many heroes as you need.
 
 
-###Requirements
+##Goals
 * ***hero*** directive restricted as element and with given template as follows: *```<img src="assets/hero.png" class="hero-position" width="300px">```*
 * ***mask*** directive restricted as element and with given template as follows: *```<img src="assets/mask.png" class="mask-position" width="62px">```*
 * ***superhero*** directive restricted as element with template that use previous directives  - you should put mask after the hero element (use directives inside template *```<div class="relative-position">....</div>```* 
 * Directives used in correct places at **index.html** (check comments)
 
-###Result
 The **superhero** should look as follows:
 
 ![alt text](app/assets/example.png "Superhero")
 
+##Setup
+You should have installed `npm`, `bower`, `grunt`  packages to run this example. First, run sequentially
+
+```
+npm install
+```
+
+```
+bower install
+```
+
+To start the application, run
+
+```
+grunt serve
+```
+
+To start unit test, run
+
+```
+grunt karma
+```
+
 ###Before you start, please refer to:
 * [angularjs-first-directive](https://egghead.io/lessons/angularjs-first-directive)
 * [write-your-first-directive](https://egghead.io/lessons/write-your-first-directive)
-
 
 Good luck!
---
+++ tests/unit/hero.spec.js
@@ -0,0 +1,82 @@
+'use strict';
+
+describe('user', function ()
+{
+    var element;
+    var scope;
+    var hero;
+    var mask;
+    var superhero;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function ($rootScope, $compile)
+    {
+        element =
+                angular.element('<div class="col-sm-offset-2 col-sm-2"><hero></hero></div><div class="col-sm-2"><superhero></superhero></div><div class="col-sm-2"><hero></hero></div>');
+
+        scope = $rootScope;
+        $compile(element)(scope);
+        scope.$digest();
+    }));
+
+
+    describe('hero directive', function ()
+    {
+        beforeEach(function ()
+        {
+            hero = element.find('hero');
+
+        });
+        it('should has "hero-position" class', function ()
+        {
+            expect(hero.find('img').hasClass('hero-position')).toBe(true);
+        });
+
+        it('should set the width of the image to 300px', function ()
+        {
+            expect((hero.find('img')).attr('width')).toBe('300px');
+        });
+    });
+
+    describe('mask directive', function ()
+    {
+        beforeEach(function ()
+        {
+            mask = element.find('mask');
+
+        });
+        it('should has "mask-position" class', function ()
+        {
+            expect(mask.find('img').hasClass('mask-position')).toBe(true);
+        });
+        it('should set the width of the image to 62px', function ()
+        {
+            expect((mask.find('img')).attr('width')).toBe('62px');
+        });
+    });
+
+    describe('superhero directive', function ()
+    {
+        beforeEach(function ()
+        {
+            superhero = element.find('superhero').find('div');
+
+        });
+        it('should has "relative-position" class', function ()
+        {
+            expect(superhero.hasClass('relative-position')).toBe(true);
+        });
+
+        it('should contain hero directive', function ()
+        {
+            expect(superhero.find('hero').find('img').hasClass('hero-position')).toBe(true);
+        });
+        it('should contain mask directive', function ()
+        {
+            expect(superhero.find('mask').find('img').hasClass('mask-position')).toBe(true);
+        });
+    });
+
+
+});
