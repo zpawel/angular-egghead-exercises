a2c1a239eb7cf327323c05e53570650593b00a68
zpawel <zpawel92@gmail.com>
2015-07-27 13:01:31 +0200

    exercise9

--- app/mouse.js
+++ app/mouse.js
@@ -1,3 +1,35 @@
 'use strict';
 
 var app = angular.module('app', []);
+
+app.directive('enter',function(){
+    return function($scope, element, attrs){
+        element.bind('mouseenter', function () {
+            element.addClass(attrs.enter);
+        })
+    }
+});
+
+app.directive('leave',function(){
+    return function($scope, element, attrs){
+        element.bind('mouseleave', function () {
+            element.removeClass(attrs.enter);
+        })
+    }
+});
+
+app.directive('dbClickOn',function(){
+    return function($scope,element,attrs){
+        element.bind('dblclick',function(){
+            element.addClass(attrs.dbClickOn);
+        })
+    }
+});
+
+app.directive('clickOn',function(){
+    return function($scope,element,attrs){
+        element.bind('click',function(){
+            element.removeClass(attrs.dbClickOn);
+        })
+    }
+});
