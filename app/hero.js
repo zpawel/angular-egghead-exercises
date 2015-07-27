377b2eef1b2859775015f9121d176bef6ffff978
zpawel <zpawel92@gmail.com>
2015-07-27 10:34:49 +0200

    exercise8

--- app/hero.js
+++ app/hero.js
@@ -1,3 +1,23 @@
 'use strict';
 
 var app = angular.module('app', []);
+
+app.directive('hero', function() {
+    return{
+        restrict: "E",
+        template:'<div> <img src="assets/hero.png" class="hero-position" width="300px"></div>'
+    }
+});
+
+app.directive('mask', function() {
+    return{
+        restrict: "E",
+        template:'<div> <img src="assets/mask.png" class="mask-position" width="62px"></div>'
+    }
+});
+app.directive('superhero', function() {
+    return{
+        restrict: "E",
+        template:'<div class="relative-position"><hero></hero><mask></mask></div>'
+    }
+});
