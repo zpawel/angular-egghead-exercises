112646b31939a0ffd0d141c518c642f7b772d8bd
zpawel <zpawel92@gmail.com>
2015-07-30 14:13:04 +0200

    exercise13

--- app/snow.js
+++ app/snow.js
@@ -7,7 +7,31 @@ app.controller('SnowCtrl', function ($scope)
     $scope.snow = [];
 
 //    addPetal function
+    $scope.addPetal= function(){
+        $scope.snow.push({petal: '*'})
+
+    },
 //    removePetal function
+    $scope.removePetal= function(){
+        $scope.snow.splice(0,1)
+    }
 });
 
 //    add enter directive
+app.directive("enter", function(){
+    return function(scope, element, attrs){
+    element.bind("mouseenter", function(){
+        scope.$apply(attrs.enter);
+        console.log(scope.snow)
+    })
+    }
+
+})
+    //app.directive("enter", function(){
+    //    return function(scope, element, attrs){
+    //        element.bind("mouseenter", function(){
+    //            scope.removePetal(snow)
+    //        })
+    //    }
+    //
+    //})
