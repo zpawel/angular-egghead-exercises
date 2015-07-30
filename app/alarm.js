9bb55ea49cbddfea33f05bfaee1e5c366d92f912
zpawel <zpawel92@gmail.com>
2015-07-30 14:28:01 +0200

    exercise14

--- app/alarm.js
+++ app/alarm.js
@@ -1,6 +1,47 @@
 var app = angular.module('app', []);
 
 //<!--add alarm directive-->
+app.directive('alarm', function(){
+    return{
+        controller: function(){
+            addCollorRed=function(){
+               alert('ColorRed alarm!')
+            }
+            addCollorYellow=function(){
+                alert('ColorYellow alarm!')
+            }
+            addCollorGreen=function(){
+                alert('ColorGreen alarm!')
+            }
+        }
+
+    }
+})
 //<!--add red directive-->
+app.directive('red', function(){
+    return{
+        link: function addCollorRed(){
+
+        }
+
+    }
+})
 //<!--add yellow directive-->
+app.directive('yellow', function(){
+    return{
+        link: function addCollorYellow(){
+
+        }
+
+    }
+})
 //<!--add green directive-->
+app.directive('green', function(){
+    return{
+        link: function addCollorGreen(){
+
+        }
+
+
+    }
+})
