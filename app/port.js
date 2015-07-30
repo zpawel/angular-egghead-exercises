68245c8aad9c7ea8dd3e4af2fc49940c49c5db53
zpawel <zpawel92@gmail.com>
2015-07-30 08:26:51 +0200

    exercise11

--- app/port.js
+++ app/port.js
@@ -29,9 +29,17 @@ app.controller('shipCtrl', function ($scope, WeatherConditions)
 app.directive('shipSender', function ()
 {
     return {
+
+        scope:{
+            customCheck: '&',
+            conditions: '@',
+        },
+
+
         template: '<div class="ship relative">' +
-                '<div class="bubble" ng-if="answer">Set sails!</div>' + //        value from the select element below should be used by captain to set boat model and passed as argument to correct function
-                '<div class="col-md-offset-1 col-md-10 align-bottom"><select class="form-control" ng-options="item for item in [\'Sloop\', \'Ship\', \'Towboat\', \'Brig\']"></select>' +
-                '<div id="checkButton" class="btn btn-info voffset">Check<!--check button--></div></div></div>'
+        '<div class="bubble" ng-if="answer">Set sails!</div>' + //        value from the select element below should be used by captain to set boat model and passed as argument to correct function
+        '<div class="col-md-offset-1 col-md-10 align-bottom">' +
+        '<select class="form-control" ng-options="item for item in [\'Sloop\', \'Ship\', \'Towboat\', \'Brig\']" ng-model="item"></select>' +
+        '<div id="checkButton" class="btn btn-info voffset" ng-click="answer=customCheck({boat:item, currentConditions:conditions})">Check<!--check button--></div></div></div>'
     };
 });
