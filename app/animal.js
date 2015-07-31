3544044483b7998360730585d837de61dc75663f
zpawel <zpawel92@gmail.com>
2015-07-31 13:11:58 +0200

    exercise16

--- app/animal.js
+++ app/animal.js
@@ -9,13 +9,14 @@ animalApp.directive('animal', function ()
         scope: {
             name: '@'
         },
+        transclude: true,
         template: '<div class="col-md-4">' +
                 '<h3>{{name}}</h3>' +
                 '<div>' +
                 '<div ng-switch on="name">' +
-                '<div ng-switch-when="dog"><div class="btn btn-default"><img src="assets/dog.png"></div></div>' +
-                '<div ng-switch-when="cat"><div class="btn btn-default"><img src="assets/cat.png"></div></div>' +
-                '<div ng-switch-when="pig"><div class="btn btn-default"><img src="assets/pig.jpg"></div></div>' +
+                '<div ng-switch-when="dog"><div class="btn btn-default"  ng-transclude><img src="assets/dog.png"></div></div>' +
+                '<div ng-switch-when="cat"><div class="btn btn-default" ng-transclude><img src="assets/cat.png" ></div></div>' +
+                '<div ng-switch-when="pig"><div class="btn btn-default" ng-transclude><img src="assets/pig.jpg" ></div></div>' +
                 '</div></div>'
     };
 });
