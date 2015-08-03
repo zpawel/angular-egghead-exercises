ddd5ff9ecf94b417b329c88c7512fb807a3219e3
zpawel <zpawel92@gmail.com>
2015-08-03 09:10:11 +0200

    exercise17

--- app/calc.js
+++ app/calc.js
@@ -3,4 +3,26 @@
 var calcApp = angular.module('calcApp', []);
 
 //SumCtrl
+calcApp.controller("SumCtrl", function ($scope) {
+   this.firstNumber =12,
+       this.secondNumber = 1,
+
+    this.add= function(firstNumber, secondNumber){
+
+        this.sum= firstNumber+ secondNumber;
+
+    }
+    return $scope.SumCtrl = this;
+})
 //SubCtrl
+calcApp.controller("SubCtrl", function ($scope) {
+    this.firstNumber =12,
+        this.secondNumber = 1,
+
+        this.subtraction= function(first, second){
+
+            this.sub= first - second;
+
+        }
+    return $scope.SubCtrl = this;
+})
