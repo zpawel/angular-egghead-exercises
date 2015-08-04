bdcb7c96cde75fcb9c6a3a1ca7b3720676a8e9a8
zpawel <zpawel92@gmail.com>
2015-08-04 14:09:35 +0200

    exercise25

--- app/app.js
+++ app/app.js
@@ -1,20 +1,42 @@
-(function ()
-{
+(function () {
     'use strict';
 
     var app = angular.module('app', ['ngRoute']);
 
     // add routing here
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/',
+            {
+                templateUrl: 'views/home.html',
+                controller: 'appCtrl'
+            })
+            .when('/details/:brand/:capacity',
+            {
+                templateUrl: 'views/details.html',
+                controller: 'routeCtrl'
+            })
+            .when('/inputs/:brand/:capacity', {
+                redirectTo: function (routeParams, path, search) {
+                    if (routeParams.brand === 'secret' || routeParams.capacity === 'secret')
+                        return '/' + 'secret';
+                }
+            })
+            .when('/inputs/secret',
+            {
+                templateUrl: 'views/secret.html',
+                controller: 'routeCtrl'
+            })
 
-    app.controller('appCtrl', function ($scope, $routeParams)
-    {
+    })
+
+    app.controller('appCtrl', function ($scope, $routeParams) {
         $scope.$root.condition = true;
         $scope.brand = $routeParams.brand;
         $scope.capacity = $routeParams.capacity;
     });
 
-    app.controller('routeCtrl', function ($scope, $routeParams)
-    {
+    app.controller('routeCtrl', function ($scope, $routeParams) {
         $scope.$root.condition = true;
         $scope.brand = $routeParams.brand;
         $scope.brand = $routeParams.brand;
