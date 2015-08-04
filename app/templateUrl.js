4200c93d6effdfa69c5ebcf5dd3fab9d631c0cc0
zpawel <zpawel92@gmail.com>
2015-08-04 12:56:50 +0200

    exercise23

--- app/templateUrl.js
+++ app/templateUrl.js
@@ -3,14 +3,20 @@
     'use strict';
     var app = angular.module('app', []);
 
-    app.run(function ()
+    app.run(function ($templateCache)
     {
+
         var template = [];
         template[0] = '<div id="default"><div>Default template:</div><h3 id="templateContent">{{order}} x {{quantity}}</h3></div>';
         template[1] =
                 '<div id="button"><div>Button template:</div>\n<div>\n    <h3 ng-click="content()" id="buttonTemplate"  class="btn btn-default">Click to see the order</h3>\n    <div ng-show="isContentVisible">\n        <h3 id="buttonContent" ng-if="quantity > 1" class="form-group">Your order: {{quantity}} {{order}}s</h3>\n        <h3 ng-if="quantity == 1" class="form-group">Your order: {{quantity}} {{order}}</h3>\n    </div>\n</div></div>';
         template[2] =
                 '<div id="table"><h4>Table template:</h4>\n<table id="tableContent" class="table table-bordered">\n    <thead>\n    <tr>\n        <th class="text-center">Order</th>\n        <th class="text-center">Quantity</th>\n    </tr>\n    </thead>\n    <tbody>\n    <tr>\n        <td>{{order}}</td>\n        <td>{{quantity}}</td>\n    </tr>\n    </tbody>\n</table></div>';
+
+
+        $templateCache.put('default',  template[0])
+        $templateCache.put('button',  template[1])
+        $templateCache.put('table',  template[2])
     });
 
     app.controller('urlCtrl', function ($scope)
@@ -19,16 +25,29 @@
         $scope.quantity = 12;
 
     });
-    app.directive('url', function ($compile)
+    app.directive('url', function ($compile, $templateCache)
     {
 
-        var link = function (scope, element)
+        var link = function (scope, element, attr)
         {
             scope.$watch(function ()
             {
 
-            }, function ()
+                return attr.template;
+
+            }, function (newValue, oldValue)
             {
+                if(newValue === 'button'){
+                   element.html( $templateCache.get('button'));
+
+                }
+                else if(newValue === 'table'){
+                    element.html($templateCache.get('table'));
+
+                }
+                else{
+                    element.html($templateCache.get('default'));
+                }
 
                 $compile(element.contents())(scope);
             });
@@ -45,5 +64,6 @@
                 order: '@', quantity: '@'
             }, link: link
         };
+
     });
 })();
