ab69fecc69870e6cbcfc03ba4d7faea83578a6cb
zpawel <zpawel92@gmail.com>
2015-08-10 10:11:34 +0200

    exercise30:

--- app/home.js
+++ app/home.js
@@ -1,18 +1,42 @@
 'use strict';
 var homeApp = angular.module('homeApp', []);
 
-// add homeApp.provider 
 
-// add homeApp.config 
+
+// add homeApp.provider 
+homeApp.provider('Home', function(){
+    var number,color;
+    return{
+        setColor: function(value){
+            color = value;
+        },
+        setNumber: function(value){
+            number = value;
+
+        },
+        $get: function(){
+            return{
+                color: color,
+                number: number % 2 ? 100 : 99
+            }
+        }
+    }
+});
+
+// add homeApp.config
+homeApp.config(function (HomeProvider){
+    HomeProvider.setColor('yellow');
+    HomeProvider.setNumber(67);
+});
 
 // remove factory 
-homeApp.factory('Home', function() {
-    var exampleNum = 67;
-    return {
-        color: 'red',
-        number: exampleNum % 2 ? 100 : 99
-    };
-});
+//homeApp.factory('Home', function() {
+//    var exampleNum = 67;
+//    return {
+//        color: 'red',
+//        number: exampleNum % 2 ? 100 : 99
+//    };
+//});
 
 homeApp.controller('HomeCtrl', function (Home)
 {
