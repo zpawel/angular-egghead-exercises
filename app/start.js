8d153adb5d9d35eba079f4b835046c533ea4b585
aniaw <ania_w@outlook.com>
2015-08-12 08:45:31 +0200

    update userList

--- app/start.js
+++ app/start.js
@@ -1,53 +1,37 @@
-(function () {
+(function ()
+{
     'use strict';
 
     var app = angular.module('app', []);
 
-    app.factory('UsersList', function () {
+    app.factory('UsersList', function ()
+    {
         var usersList = {};
-        usersList.list = [
-            {
-                name: 'Adam', lastName: 'Hart', country: 'Poland'
-            },
-            {
-                name: 'Antonio', lastName: 'Turner', country: 'England'
-            },
-            {
-                name: 'Jimmy', lastName: 'Wright', country: 'USA'
-            },
-            {
-                name: 'Shirley', lastName: 'Harper', country: 'Italian'
-            },
-            {
-                name: 'Randy', lastName: 'Watson', country: 'Greece'
-            },
-            {
-                name: 'Gregory', lastName: 'Banks', country: 'German'
-            },
-            {
-                name: 'Martha', lastName: 'Lawson', country: 'China'
-            },
-            {
-                name: 'Eugene', lastName: 'Stanley', country: 'Cuba'
-            },
-            {
-                name: 'Randy', lastName: 'Reyes', country: 'Egypt'
-            },
-            {
-                name: 'Christopher', lastName: 'Ferguson', country: 'France'
-            }
-        ];
+        usersList.list = [{
+            name: 'Adam', lastName: 'Hart', country: 'Poland'
+        }, {
+            name: 'Antonio', lastName: 'Turner', country: 'England'
+        }, {
+            name: 'Jimmy', lastName: 'Wright', country: 'USA'
+        }, {
+            name: 'Shirley', lastName: 'Harper', country: 'Italian'
+        }, {
+            name: 'Randy', lastName: 'Watson', country: 'Greece'
+        }, {
+            name: 'Gregory', lastName: 'Banks', country: 'German'
+        }];
 
         return usersList;
 
     });
 
-    app.controller('ViewCtrl', function (UsersList) {
+    app.controller('ViewCtrl', function (UsersList)
+    {
         this.users = UsersList;
-        this.toggle = function (index) {
+        this.toggle = function (index)
+        {
             //complete function
             this.users.list[index].enable = !this.users.list[index].enable;
         };
     });
-})
-();
+})();
